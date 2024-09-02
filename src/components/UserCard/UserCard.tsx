fc52930999e0f74ff4fd062600fb5b33851a8656
SanghyunLee <lgw9736@naver.com>
2024-09-02 20:26:46 +0900

    🐛 올인 관련 수정

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -6,26 +6,50 @@ import React from 'react';
 import { User } from '../../features/single/singleSlice';
 
 type Props = {
-    isLeft : boolean,
-    user : User
+    isLeft: boolean,
+    user: User
 }
 
-export default function UserCard({isLeft,user }: Props) {
+export default function UserCard({ isLeft, user }: Props) {
     return (
         <div className={`flex items-center p-2 ${user.isTurn ? 'bg-third rounded-md' : ''}`}>
-            {!isLeft && <div>
-                <p>{user.currentRoundBet}</p>
-                <p>{user.currentRoundBehavior}</p>
-            </div>}
-            <div className='flex flex-col items-center '>
+            {!isLeft && (
+                <div>
+                    <p>{user.currentRoundBet}</p>
+                    {user.currentRoundBehavior !== 0 && (
+                        <p className={
+                            typeof user.currentRoundBehavior === 'string'
+                                ? user.currentRoundBehavior === 'DIE'
+                                    ? 'text-red-500'
+                                    : 'text-black'
+                                : 'text-blue-500'
+                        }>
+                            {user.currentRoundBehavior}
+                        </p>
+                    )}
+                </div>
+            )}
+            <div className='flex flex-col items-center'>
                 <FontAwesomeIcon icon={faUser} size='2xl' />
                 <p>{user.name}</p>
                 <p>{user.currentScore}</p>
             </div>
-            {isLeft && <div>
-                <p>{user.currentRoundBet}</p>
-                <p>{user.currentRoundBehavior}</p>
-            </div>}
+            {isLeft && (
+                <div>
+                    <p>{user.currentRoundBet}</p>
+                    {user.currentRoundBehavior !== 0 && (
+                        <p className={
+                            typeof user.currentRoundBehavior === 'string'
+                                ? user.currentRoundBehavior === 'DIE'
+                                    ? 'text-red-500'
+                                    : 'text-black'
+                                : 'text-blue-500'
+                        }>
+                            {user.currentRoundBehavior}
+                        </p>
+                    )}
+                </div>
+            )}
         </div>
     )
 }
--- src/features/single/singleSlice.ts
+++ src/features/single/singleSlice.ts
@@ -7,7 +7,7 @@ export type User = {
 	currentScore: number;
 	isTurn: boolean;
 	currentRoundBet: number;
-	currentRoundBehavior: string | number | null;
+	currentRoundBehavior: string | number ;
 	isDie: boolean;
 };
 
@@ -52,12 +52,22 @@ function turnOver(state: State) {
 			// 죽지 않은 사람만 추가
 			if(state.users[value].isDie===false){
 				state.userTurnOrder.push(value);
-				state.users[value].currentRoundBehavior=null;
+				// ALL-IN 은 다음 턴이 되어도 유지되게
+				if(state.users[value].currentRoundBet===state.users[value].currentScore){
+					state.users[value].currentRoundBehavior='ALL-IN';
+				}else{
+					state.users[value].currentRoundBehavior=0;
+				}
 			}
 		}
 	}
+	// 모두 죽었을 경우
+	if(state.userTurnOrder.length===0){
+
+	}else{
 	// 그 다음 유저의 턴을 true로 설정합니다.
 	state.users[state.userTurnOrder[0]].isTurn = true;
+	}
 }
 
 export const singleSlice = createSlice({
@@ -130,10 +140,18 @@ export const singleSlice = createSlice({
 			}
 			// call
 			else {
-				state.currentRoundTotalScore +=
-					state.requiredCallScore - currentUser.currentRoundBet;
-				currentUser.currentRoundBet = state.requiredCallScore;
-				currentUser.currentRoundBehavior = "CALL";
+				// 콜 필요 금액이 자신의 자산보다 높을 경우
+				if(currentUser.currentScore < state.requiredCallScore){
+					currentUser.currentRoundBehavior = "ALL-IN";
+					state.currentRoundTotalScore +=currentUser.currentScore - currentUser.currentRoundBet;
+					currentUser.currentRoundBet = currentUser.currentScore;
+					currentUser.isDie=true;
+				}
+				else{
+					currentUser.currentRoundBehavior = "CALL";
+					state.currentRoundTotalScore += state.requiredCallScore - currentUser.currentRoundBet;
+					currentUser.currentRoundBet = state.requiredCallScore;
+				}
 			}
 			turnOver(state);
 		},
@@ -146,6 +164,7 @@ export const singleSlice = createSlice({
 				if (
 					state.requiredCallScore < currentUser.currentRoundBehavior + currentUser.currentRoundBet
 				) {
+					currentUser.isTurn=false;
 					// 새로운 userTurnOrder에서 첫 번째 유저의 인덱스를 userTurnStandard를 기준으로 찾습니다.
 					state.userTurnIndex = state.userTurnStandard.indexOf(
 						state.userTurnOrder[0]
@@ -175,43 +194,83 @@ export const singleSlice = createSlice({
 					currentUser.currentRoundBet +=
 						currentUser.currentRoundBehavior;
 
-					// // 해당 유저의 이전 행동에 추가
-					// currentUser.currentRoundBehavior = `Raise ${currentUser.currentRoundBehavior}`;
-
 					// 턴 넘기기
-					currentUser.isTurn=false;
 					state.users[state.userTurnOrder[0]].isTurn=true;
 				}
+			}
+			// ALL-IN 일때, BET 했을 경우 
+			else if(currentUser.currentRoundBehavior === 'ALL-IN'){
+				currentUser.isTurn=false;
+					// 새로운 userTurnOrder에서 첫 번째 유저의 인덱스를 userTurnStandard를 기준으로 찾습니다.
+					state.userTurnIndex = state.userTurnStandard.indexOf(
+						state.userTurnOrder[0]
+					);
+
+					// 유저 턴 변경 및 인덱스 변경
+					state.userTurnOrder = [];
+
+					// userTurnStandard에서 bet을 한 유저의 인덱스(userTurnIndex)를 기준으로 배열을 갱신합니다.
+					for (let i = 0; i < state.userTurnStandard.length - 1; i++) {
+						const value =
+							state.userTurnStandard[(state.userTurnIndex + i + 1) % state.userTurnStandard.length];
+						// 안죽은 사람만 추가
+						if(state.users[value].isDie===false){
+							state.userTurnOrder.push(value);
+						}
+					}
+
+					// 필요 콜 금액 변경
+					state.requiredCallScore = currentUser.currentScore;
+
+					// 해당 라운드 금액 추가
+					state.currentRoundTotalScore +=currentUser.currentScore;
+
+					// 해당 유저의 이번 라운드 벳 추가
+					currentUser.currentRoundBet =currentUser.currentScore;
+
+					// 턴 넘기기
+					state.users[state.userTurnOrder[0]].isTurn=true;
 			}
 		},
 		// Die 눌렀을 때
 		dieUser: (state) => {
-			state.users[state.userTurnOrder[0]].isDie = true;
-			state.users[state.userTurnOrder[0]].currentRoundBehavior = "DIE";
-			turnOver(state);
+			if(state.userTurnOrder.length>0){
+				state.users[state.userTurnOrder[0]].isDie = true;
+				state.users[state.userTurnOrder[0]].currentRoundBehavior = "DIE";
+				turnOver(state);
+			}
 		},
 		// 현재 턴 유저의 currentRoundBet 추가 - 100,500,1000 눌렀을 때
-		incrementUserCurrentRoundBehavior: (state, action) => {
+		incrementUserCurrentRoundBehavior: (state, action: { payload: number }) => {
+			// 해당 유저
 			const user=state.users[state.userTurnOrder[0]];
-			const betScore = user.currentRoundBehavior + action.payload;
-			// 베팅한 금액이 유저가 가진 금액보다 많을 경우
-			if(betScore>user.currentScore){
-				user.currentRoundBehavior=user.currentScore;
-			} else{
-				user.currentRoundBehavior += action.payload;
+			if(typeof user.currentRoundBehavior ==='number' ){
+				// 현재 라운드 금액
+				const betScore = user.currentRoundBehavior + action.payload;
+				// 베팅한 금액이 유저가 가진 금액보다 많을 경우
+				if(betScore>user.currentScore){
+					// user.currentRoundBehavior=user.currentScore - user.currentRoundBet;
+					user.currentRoundBehavior='ALL-IN';
+				} else if(betScore + user.currentRoundBet > user.currentScore){
+					// user.currentRoundBehavior=user.currentScore - user.currentRoundBet;
+					user.currentRoundBehavior='ALL-IN';
+				}
+				else{
+					user.currentRoundBehavior += action.payload;
+				}
 			}
 		},
 		// 배열에 있는 유저들에게 해당 라운드 금액 나눠주기
-		divideRoundScoreToWinner: (state,action) => {
+		divideRoundScoreToWinner: (state,action : {payload : number[]}) => {
 			const selectedUserIndexes = action.payload;
-            const totalScore = state.currentRoundTotalScore;
+            const totalScore = state.currentRoundTotalScore; //3050
 
             // 나누어 줄 수의 크기를 계산합니다.
             const scoreToAdd = totalScore / selectedUserIndexes.length;
 
             // 선택된 유저들에 대해서 점수를 나눠서 추가합니다.
             selectedUserIndexes.forEach((index: number) => {
-                state.users[index].currentScore += (scoreToAdd-state.users[index].currentRoundBet);
+                state.users[index].currentScore += (scoreToAdd-state.users[index].currentRoundBet); //3050 - 1050 = 2000
 				state.users[index].currentRoundBet = 0;
             });
 
@@ -231,7 +290,8 @@ export const singleSlice = createSlice({
 				const value =state.userTurnStandard[(state.userTurnIndex + i) % state.userTurnStandard.length];
 				state.userTurnOrder.push(value); // 턴 다시 추가
 				state.users[value].isTurn=false; // 턴 내용 초기화
-				state.users[value].currentRoundBehavior=null; // 이전에 한 행동 초기화
+				state.users[value].isDie=false; // 턴 내용 초기화
+				state.users[value].currentRoundBehavior=0; // 이전에 한 행동 초기화
 			}
 			// 첫번째 유저 턴 주기
 			state.users[state.userTurnOrder[0]].isTurn=true;
@@ -246,7 +306,7 @@ export const singleSlice = createSlice({
 		// 모든 유저의 currentBehavior 초기화
 		resetAllUserCurrentRoundBehavior: (state) => {
 			state.users.forEach((user)=>{
-				user.currentRoundBehavior=null;
+				user.currentRoundBehavior=0;
 			})
 		},
 		// 라운드 인당 콜 금액 초기화
@@ -256,12 +316,18 @@ export const singleSlice = createSlice({
 		// 라운드 시작할 때, 초기 금액 설정
 		setInitialRoundSetting : (state) =>{
 			const initialBet=state.initialBet;
+			let survivedUserCount = 0;
 
 			state.users.forEach((user)=>{
-				user.currentRoundBet=initialBet;
+				if(user.currentScore>=initialBet){
+					user.currentRoundBet=initialBet;
+					survivedUserCount++;
+				}else{
+					user.isDie=true;
+				}	
 			})
 
-			state.currentRoundTotalScore=initialBet * state.users.length;
+			state.currentRoundTotalScore=initialBet * survivedUserCount;
 			state.requiredCallScore=initialBet;
 		},
 		// 라운드 수 늘리기
