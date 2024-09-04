635bab304a917875c1c76fbb3fcc667c953395c4
SanghyunLee <lgw9736@naver.com>
2024-09-04 14:40:22 +0900

    ✅ 방 나갈때 게임 결과 복사되는 기능 추가

--- src/components/SingleResultModal/SingleResultModal.tsx
+++ src/components/SingleResultModal/SingleResultModal.tsx
@@ -1,12 +1,8 @@
-import { Select, Checkbox, Button, Modal, Input } from "antd";
+import { Modal } from "antd";
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import useUserposition from "../../hooks/useUserPostition";
-import UserNameInput from "../UserNameInput/UserNameInput";
-import { useSelector, useDispatch } from 'react-redux';
-import { setUserCount, setUsers, setInitialBet, setUserTurnOrder, setInitialRoundSetting } from "../../features/single/singleSlice";
+import { useSelector } from 'react-redux';
 import { RootState } from "../../app/store";
-import { User } from "../../features/single/singleSlice";
 
 type Props = {
     isModalOpen: boolean,
@@ -14,13 +10,26 @@ type Props = {
 }
 
 export default function SingleResultModal({ isModalOpen, setIsModalOpen, }: Props) {
-    const navigate=useNavigate();
+    const navigate = useNavigate();
 
     const users = useSelector((state: RootState) => state.single.users);
 
-    const handleExitClick = () =>{
-        
-        navigate('/single')
+    const handleExitClick = () => {
+        // 복사할 텍스트 생성
+        const copiedText = users.map(user => {
+            const scoreDifference = user.currentScore - user.initialScore;
+            return `${user.name}: ${scoreDifference > 0 ? `+${scoreDifference}` : scoreDifference}`;
+        }).join('\n'); // 각 사용자 정보를 줄바꿈으로 연결
+
+        // 클립보드에 복사
+        navigator.clipboard.writeText(copiedText).then(() => {
+            alert('결과가 클립보드에 복사되었습니다!');
+        }).catch(err => {
+            console.error('복사 실패: ', err);
+        });
+
+        // '/single' 페이지로 이동
+        navigate('/single');
     }
 
     return (
